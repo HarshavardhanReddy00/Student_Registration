3ad85eb9872d46cabb10e87da959dc3d5592dbf9
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-01-20 16:32:34 +0530

    some changes done

--- script.js
+++ script.js
@@ -14,4 +14,41 @@ form.addEventListener("submit", function(event)) {
     const email = studentEmail.value.trim();
     const contact = studentContact.value.trim();
     
-}
+    if (!name || !id || !email || !contact) {
+    alert("All fields are required");
+    return;
+    }
+
+    if (!/^[A-Za-z ]+$/.test(name)) {
+    alert("Name should contain only letters");
+    return;
+    }
+    
+    if (!/^\d+$/.test(id)) {
+        alert("Student ID should contain only numbers");
+        return;
+    }
+
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+    alert("Enter a valid email address");
+    return;
+    }
+
+    if (!/^\d{10,}$/.test(contact)) {
+    alert("Contact number must be at least 10 digits");
+    return;
+    }
+
+    const student = { name, id, email, contact };
+
+    if (editIndex === null) {
+     studentRecords.push(student);
+    } else {
+     studentRecords[editIndex] = student;
+     editIndex = null;
+    }
+
+     localStorage.setItem("students", JSON.stringify(studentRecords));
+     form.reset();
+     renderTable();
+});
